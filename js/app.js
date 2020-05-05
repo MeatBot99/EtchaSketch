e2434d286afdc216bc143f55d6ad26e5296057f2
Nathan Heck <nathan.heck@gmail.com>
2020-05-05 15:27:00 -0600

    Starting project. Initial code implemented. Much work to do

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 //App.js//
+const init = {
+    start: function(){
+        let container = document.getElementById("container");
+        console.log("Container created?");},
+    addDivs: function(){
+        for(let i = 0; i<10; i++){
+            for (let j = 0; j<10; j++){
+                let newDiv = document.createElement("div");
+                newDiv.setAttribute("id", j*i+1)
+                container.appendChild(newDiv);
+                console.log(newDiv.id)
+            }
+        }
+    }
+};
+
+init.start();
+init.addDivs();
