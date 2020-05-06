c12d740a3c5d1411c6b1286d66bc5d96e472e7e8
Nathan Heck <nathan.heck@gmail.com>
2020-05-06 14:48:24 -0600

    Some changes but nothing significant

--- js/app.js
+++ js/app.js
@@ -1,19 +1,21 @@
-//App.js//
 const init = {
-    start: function(){
+    parameterSize: 16, 
+    addDivs: function(x){
         let container = document.getElementById("container");
-        console.log("Container created?");},
-    addDivs: function(){
-        for(let i = 0; i<10; i++){
-            for (let j = 0; j<10; j++){
+        for(let i = 0; i<x; i++){
+            for (let j = 0; j<x; j++){
                 let newDiv = document.createElement("div");
-                newDiv.setAttribute("id", j*i+1)
+                newDiv.setAttribute("class", "square");
+                newDiv.setAttribute("style", "height: "+x+"px");
                 container.appendChild(newDiv);
-                console.log(newDiv.id)
             }
         }
+    },
+    changeParameter: function(){
+        init.parameterSize = 5;
+        init.addDivs(init.parameterSize);
+        //this doesnt do anything that I want it to just yet
     }
 };
 
-init.start();
-init.addDivs();
+init.addDivs(init.parameterSize);
